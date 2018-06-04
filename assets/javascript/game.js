3db081128780561210c00632d27434b62a6fada8
MeepyMay <meepymay@gmail.com>
2018-06-04 03:17:32 -0400

    completed game with star trek voyager theme, removed the modal windows for now and included audio files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,32 @@
 var wins = 0;
-var wordOptions = ["apple", "pear", "plum", "banana", "strawberry"];
+var wordOptions = ["tuvok", "janeway", "seven", "chakotay", "neelix", "doctor", "kes", "torres", "paris"];
+var titleAudio = new Audio('assets/audio/main-title-short.mp3');
+var chakotayAudio = new Audio('assets/audio/chakotay.mp3');
+var doctorAudio = new Audio('assets/audio/doctor.mp3');
+var janewayAudio = new Audio('assets/audio/janeway.mp3');
+var kesAudio = new Audio('assets/audio/kes.mp3');
+var neelixAudio = new Audio('assets/audio/neelix.mp3');
+var parisAudio = new Audio('assets/audio/paris.mp3');
+var sevenAudio = new Audio('assets/audio/seven.wav');
+var torresAudio = new Audio('assets/audio/torres.mp3');
+var tuvokAudio = new Audio('assets/audio/tuvok.mp3');
 
 
 function game() {
     //wordOptions is our word array. gameAnswer chooses a word randomly from wordOptions.
     var gameAnswer = wordOptions[Math.floor(Math.random() * wordOptions.length)];
     var remainingLetters = gameAnswer.length;
-
     //
-    var winsDiv = document.getElementById('wins');
-
-    //
-    var gameAnswerArray = [];
-    var answerDiv = document.getElementById('answer');
-    
-    //
-    var displayGuessesArray = [];
-    var guessesDiv = document.getElementById('guesses');
-
-    //
-    var isCorrectOrRepeated;
     var remainingGuesses = 5;
+    var gameAnswerArray = [];
+    var displayGuessesArray = [];
+    var isCorrectOrRepeated;
+    //
+    var clueDiv = document.getElementById('clue');
+    var winsDiv = document.getElementById('wins');
+    var answerDiv = document.getElementById('answer');
+    var photoDiv = document.getElementById('answerPhoto');
+    var guessesDiv = document.getElementById('guesses');
     var remainingDiv = document.getElementById('remaining');
 
     //this for loop initializes the blank spaces for every letter of the word
@@ -30,10 +36,11 @@ function game() {
     //
     var displayString = gameAnswerArray.join(" ");
     //
-    winsDiv.innerHTML = "<p>Wins: " + wins + "</p>";
-    answerDiv.innerHTML = "<p>Word: </p>" + displayString;
-    guessesDiv.innerHTML = "<p>Letters Already Guessed:</p>" + (displayGuessesArray.join(" "));
-    remainingDiv.innerHTML = "<p>Number of Guesses Remaining:</p>" + remainingGuesses; 
+    clueDiv.innerHTML = "<h3>Theme: Characters</h3>";
+    winsDiv.innerHTML = "<h3>Wins: " + wins + "</h3>";
+    answerDiv.innerHTML = "<h3>Word: " + displayString + "</h3>";
+    guessesDiv.innerHTML = "<h3>Letters Already Guessed: " + (displayGuessesArray.join(" ")) + "</h3>";
+    remainingDiv.innerHTML = "<h3>Number of Guesses Remaining: " + remainingGuesses + "</h3>"; 
 
     document.onkeyup = function(event) {
 
@@ -54,21 +61,20 @@ function game() {
                         
             if (displayGuessesArray.toString().includes((userInput.toUpperCase()))){
 
-            alert("You have already guessed '" + userInput + ".' Try a different letter.");
             isCorrectOrRepeated = true;           
 
             }
             else {
                 displayGuessesArray.push(" " + userInput.toUpperCase());
-                guessesDiv.innerHTML = "<p>Letters Already Guessed:</p>" + (displayGuessesArray.join(" "));
+                guessesDiv.innerHTML = "<h3>Letters Already Guessed: " + (displayGuessesArray.join(" ")) + "</h3>";
             }
-            
+
             for (var i = 0; i < gameAnswer.length; i++) {
                 if (gameAnswer[i] === userInput)
                 {
                     gameAnswerArray[i] = userInput;
                     displayString = gameAnswerArray.join(" ");
-                    answerDiv.innerHTML = "<p>Word: </p>" + displayString;
+                    answerDiv.innerHTML = "<h3>Word: " + displayString + "</h3>";
     
                     remainingLetters--;
                     isCorrectOrRepeated = true;      
@@ -78,28 +84,77 @@ function game() {
             if (gameAnswer === gameAnswerArray.join(""))
             {
                 wins++;
-                winsDiv.innerHTML = "<p>Wins: " + wins + "</p>";
-                alert("You win!");
+                winsDiv.innerHTML = "<h3>Wins: " + wins + "</h3>";
+                // $('#winModal').modal('show');
+
+                switch(gameAnswer) {
+                    case "tuvok":
+                        photoDiv.innerHTML = "<img src='assets/images/tuvok.gif' class='rounded mx-auto d-block' class='img-fluid' style='width: 280px; height: 280px'>"
+                        + "<h2>Tuvok</h2>";
+                        tuvokAudio.play();
+                        break;
+                    case "janeway":
+                        photoDiv.innerHTML = "<img src='assets/images/janeway.gif' class='rounded mx-auto d-block' class='img-fluid' style='width: 280px; height: 280px'>"
+                        + "<h2>Janeway</h2>";
+                        janewayAudio.play();
+                        break;
+                    case "seven":
+                        photoDiv.innerHTML = "<img src='assets/images/seven.gif' class='rounded mx-auto d-block' class='img-fluid' style='width: 280px; height: 280px'>"
+                        + "<h2>Seven</h2>";
+                        sevenAudio.play();
+                        break;
+                    case "chakotay":
+                        photoDiv.innerHTML = "<img src='assets/images/chakotay.gif' class='rounded mx-auto d-block' class='img-fluid' style='width: 280px; height: 280px'>"
+                        + "<h2>Chakotay</h2>";
+                        chakotayAudio.play();
+                        break;
+                    case "neelix":
+                        photoDiv.innerHTML = "<img src='assets/images/neelix.jpg' class='rounded mx-auto d-block' class='img-fluid' style='width: 280px; height: 280px'>"
+                        + "<h2>Neelix</h2>";
+                        neelixAudio.play();
+                        break;
+                    case "doctor":
+                        photoDiv.innerHTML = "<img src='assets/images/doctor.gif' class='rounded mx-auto d-block' class='img-fluid' style='width: 280px; height: 280px'>"
+                        + "<h2>Doctor</h2>";
+                        doctorAudio.play();
+                        break;
+                    case "kes":
+                        photoDiv.innerHTML = "<img src='assets/images/kes.gif' class='rounded mx-auto d-block' class='img-fluid' style='width: 280px; height: 280px'>"
+                        + "<h2>Kes</h2>";
+                        kesAudio.play();
+                        break;
+                    case "torres":
+                        photoDiv.innerHTML = "<img src='assets/images/torres.gif' class='rounded mx-auto d-block' class='img-fluid' style='width: 280px; height: 280px'>"
+                        + "<h2>Torres</h2>";
+                        torresAudio.play();
+                        break;
+                    case "paris":
+                        photoDiv.innerHTML = "<img src='assets/images/paris.gif' class='rounded mx-auto d-block' class='img-fluid' style='width: 280px; height: 280px'>"
+                        + "<h2>Paris</h2>";
+                        parisAudio.play();
+                        break;
+                }
+
                 remainingGuesses = 5;
                 game();
             }
 
             if (isCorrectOrRepeated){
-                remainingDiv.innerHTML = "<p>Number of Guesses Remaining:</p>" + remainingGuesses; 
+                remainingDiv.innerHTML = "<h3>Number of Guesses Remaining: " + remainingGuesses + "</h3>"; 
             }
             else {
                 remainingGuesses--;
-                remainingDiv.innerHTML = "<p>Number of Guesses Remaining:</p>" + remainingGuesses; 
+                remainingDiv.innerHTML = "<h3>Number of Guesses Remaining: " + remainingGuesses + "</h3>"; 
             }
 
             if (remainingGuesses <= 0){
-                alert("You lose!");
+                // $('#loseModal').modal('show');
                 game();
             }   
         }
-        else{
-            alert("Please enter a letter. Numbers, spaces, or symbols are not valid.");
-        }        
+        // else{
+        //     $('#letterModal').modal('show');
+        // }        
     };
 
 }
